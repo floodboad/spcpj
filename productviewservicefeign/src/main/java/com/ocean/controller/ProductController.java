5be5528c84ef82f70f76ebccfbd43f64dca7f92b
oceanyjs <oceanyjs@backenyang.local>
2020-10-13 18:50:25 +0800

    config server

--- productviewservicefeign/src/main/java/com/ocean/controller/ProductController.java
+++ productviewservicefeign/src/main/java/com/ocean/controller/ProductController.java
@@ -2,16 +2,23 @@ package com.ocean.controller;
 
 import com.ocean.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ProductController {
     @Autowired
     ProductService productService;
 
+    @Value("${version}")
+    String version;
+
     @RequestMapping("/products")
     public Object products(){
+        System.out.println(version + "----version");
         return productService.listProducts();
     }
 }
